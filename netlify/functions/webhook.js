1cefa3436017f21dced5a60a65dd59ba3ddf7834
sawayamakouji <sawayamakouji@gmail.com>
2024-05-31 11:38:31 +0900

    5

--- netlify/functions/webhook.js
+++ netlify/functions/webhook.js
@@ -8,18 +8,25 @@ exports.handler = async function(event, context) {
         };
     }
 
-    const eventData = JSON.parse(event.body);
+    try {
+        const eventData = JSON.parse(event.body);
 
-    if (eventData.type === 'participant_joined') {
-        participants.push(eventData.participant);
-    } else if (eventData.type === 'participant_left') {
-        participants = participants.filter(p => p.id !== eventData.participant.id);
+        if (eventData.type === 'participant_joined') {
+            participants.push(eventData.participant);
+        } else if (eventData.type === 'participant_left') {
+            participants = participants.filter(p => p.id !== eventData.participant.id);
+        }
+
+        return {
+            statusCode: 200,
+            body: 'Webhook received',
+        };
+    } catch (error) {
+        return {
+            statusCode: 400,
+            body: `Error parsing JSON: ${error.message}`,
+        };
     }
-
-    return {
-        statusCode: 200,
-        body: 'Webhook received',
-    };
 };
 
 exports.participants = participants;
